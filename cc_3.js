92dd5c0afcedfd092a0ac9a9c9ad45c06784543a
Kamali Hicks <kamalikh3@gmail.com>
2025-02-01 14:23:25 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1 Product Price Management
+// Set prices, add a new price, remove last price, & log updated prices to console
+let prices = [30, 45, 60, 72, 3];
+prices.push(55);
+prices.pop();
+console.log("Updated Price List:", prices);
