1c367e38f2c9e2357ca11537caf38f0db45f464f
Kamali Hicks <kamalikh3@gmail.com>
2025-02-01 15:22:13 -0500

    Included line comments

--- cc_3.js
+++ cc_3.js
@@ -1,15 +1,16 @@
 // Task 1 Product Price Management
-// Set prices, add a new price, remove last price, & log updated prices to console
-let prices = [30, 45, 60, 72, 3];
-prices.push(55);
-prices.pop();
-console.log("Updated Price List:", prices);
+
+let prices = [30, 45, 60, 72, 3]; // Set prices
+prices.push(55); // Add new price
+prices.pop(); // Remove last price
+console.log("Updated Price List:", prices); // Log updated price to console
 
 // Task 2 Modifying Customer Orders
-let order = [50, 73, 25 ,18, 33];
+
+let order = [50, 73, 25 ,18, 33]; // Declare array of orders
 order[2] += 5;
-const total = order.reduce ((sum, order) => sum + order, 0);
-console.log(total);
+const total = order.reduce ((sum, order) => sum + order, 0); // Increased third order quantity by 5 and summed the total number
+console.log("Total Order Quantity" + total); // Log total to console
 
 // Task 3 Employee Peformance Tracking 
 
@@ -18,9 +19,10 @@ let employee = {
     role: "Supreme Leader",
     performanceScore: 72,
     isActive: "Yes"
-};
-employee.performanceScore = 76;
-employee.promotionEligible = false;
+}; // Declare object with employee information
+employee.performanceScore = 76; // Updated performance score
+employee.promotionEligible = false; // Add new proprty with boolean value
+console.log(employee) // Log employee to console
 
 
 
